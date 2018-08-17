6ac01a3f2c7d00bf83c22af6cf20682a4bc085f4
paaupereda@gmail.com <paaupereda@gmail.com>
2018-08-17 16:37:38 -0300

    separa código colores

--- R/graficos.R
+++ R/graficos.R
@@ -14,32 +14,6 @@
 
 library(ggplot2)
 
-paleta <- c("#BACB33",   # VERDE
-            "#F7941E",   # Naranja
-            "#999B9E",   # Gris
-            "#ffcc00",   # Amarillo
-            "#2974B4",  # Azul
-            "#538fc3", #Violeta
-            "#d0400a", #ROJO
-            "#0a98cc", #celeste
-            "#94a228", #verde2
-            "#ffe066", #AMARILLO2
-            "#865eae", #VIOLETA2
-            "#a7b62d") 
-
-VERDE <- paleta[1]
-NARANJA <- paleta[2]
-GRIS <- paleta[3]
-AMARILLO <- paleta[4]
-AZUL <- paleta[5]
-VIOLETA <- paleta[6]
-ROJO <- paleta[7]
-CELESTE <- paleta[8]
-AMARILLO2 <- paleta[10]
-VIOLETA2 <- paleta[11]
-VERDE2 <- paleta[12]
-
-
 sysfonts::font_add("MyriadPro", "MyriadPro-Regular.otf")
 
 get_theme_palette <- function() {
