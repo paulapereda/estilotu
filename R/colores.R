faf9afdb545325062dbd43d21a3c67857ae00e4d
paaupereda@gmail.com <paaupereda@gmail.com>
2018-08-21 17:42:58 -0300

    separa código colores

---
+++ R/colores.R
@@ -0,0 +1,13 @@
+paleta <- c("#BACB33",   # 1. VERDE
+            "#F7941E",   # 2. NARANJA
+            "#999B9E",   # 3. GRIS
+            "#2974B4",   # 4. AZUL
+            "#ffcc00",   # 5. AMARILLO
+            "#a7b62d")   # 6. VERDE2
+
+VERDE <- paleta[1]
+NARANJA <- paleta[2]
+GRIS <- paleta[3]
+AZUL<- paleta[4]
+AMARILLO <- paleta[5]
+VERDE2 <- paleta[6]
