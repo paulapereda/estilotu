d1c768b45346bee3ca53b0351c314b64d961be5a
paaupereda@gmail.com <paaupereda@gmail.com>
2018-11-16 09:51:26 -0300

    agrega colores OP

--- R/colores.R
+++ R/colores.R
@@ -4,7 +4,43 @@ paleta <- c("#BACB33",   # 1. VERDE
             "#2974B4",   # 4. AZUL
             "#ffcc00",   # 5. AMARILLO
             "#a7b62d",   # 6. VERDE2
-            "#c0504d")   # 7. ROJO
+            "#c0504d",   # 7. ROJO
+            "#538fc3",   #COLORES VIR
+            "#94a228",   #COLORES VIR
+            "#de7953",   #COLORES VIR
+            "#f57f17",   #COLORES VIR
+            "#d0400a",   #COLORES VIR
+            "#ffe066",   #COLORES VIR
+            "#94b9d9",   #COLORES VIR
+            "#fb8c00",   #COLORES VIR
+            "#616161",   #COLORES VIR
+            "#d0400a",   #COLORES VIR
+            "#ffe066",   #COLORES VIR
+            "#94b9d9",   #COLORES VIR
+            "#fb8c00",   #COLORES VIR
+            "#616161",   #COLORES VIR
+            "#d0400a",   #COLORES VIR
+            "#ffe066",   #COLORES VIR
+            "#94b9d9",   #COLORES VIR
+            "#fb8c00",   #COLORES VIR
+            "#616161",   #COLORES VIR
+            "#d0400a",   #COLORES VIR
+            "#ffe066",   #COLORES VIR
+            "#94b9d9",   #COLORES VIR
+            "#fb8c00",   #COLORES VIR
+            "#616161",   #COLORES VIR
+            "#d0400a",   #COLORES VIR
+            "#ffe066",   #COLORES VIR
+            "#94b9d9",   #COLORES VIR
+            "#fb8c00",   #COLORES VIR
+            "#616161",   #COLORES VIR
+            "#0a98cc",   #COLORES VIR
+            "#865eae",   #COLORES VIR
+            "#c1c3c4",   #COLORES VIR
+            "#afb42b",   #COLORES VIR
+            "#f9b461",   #COLORES VIR
+            "#c7d55b",   #COLORES VIR
+            "#ffa726")   #COLORES VIR
 
 VERDE <- paleta[1]
 NARANJA <- paleta[2]
